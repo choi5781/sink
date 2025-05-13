cbc718aec79cfbcb745bb2f72ce24ec3241fd861
choi5781 <46472162+choi5781@users.noreply.github.com>
2025-05-13 17:18:52 +0900

    Update Twitter.vue

--- components/home/Twitter.vue
+++ components/home/Twitter.vue
@@ -1,1 +1,10 @@
+<script setup>
+import { ArrowRight } from 'lucide-vue-next'
+import { XIcon } from 'vue3-simple-icons'
 
+const { twitter } = useAppConfig()
+</script>
+
+<template>
+
+</template>
