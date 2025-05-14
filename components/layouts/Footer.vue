dda9f79978f9121e87cd20ae8c1a329c07a5859e
choi5781 <46472162+choi5781@users.noreply.github.com>
2025-05-14 14:14:19 +0900

    Update Footer.vue

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -8,19 +8,19 @@ const { title, email, telegram, blog, twitter, mastodon, github } = useAppConfig
   <section class="md:pt-6">
     <div class="container flex flex-col items-center py-8 mx-auto sm:flex-row">
       <a
-        href="https://sink.cool"
+        href="https://tou.kr"
         class="text-xl font-black leading-none text-gray-900 select-none dark:text-gray-100 logo"
         :title="title"
       >{{ title }}</a>
-      <a
+      <!--<a
         class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0"
         href="https://html.zone"
         target="_blank"
         title="HTML.ZONE"
-      >
-        &copy; {{ new Date().getFullYear() }} Products of HTML.ZONE
-      </a>
-      <span
+      >-->
+        &copy; {{ new Date().getFullYear() }}
+      <!--</a>-->
+      <!--<span
         class="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start"
       >
         <a
@@ -85,7 +85,7 @@ const { title, email, telegram, blog, twitter, mastodon, github } = useAppConfig
             class="w-6 h-6"
           />
         </a>
-      </span>
+      </span>-->
     </div>
   </section>
 </template>
