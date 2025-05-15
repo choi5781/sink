35c302ca3c2bf3088bd0415e5e677911ba30a593
choi5781 <46472162+choi5781@users.noreply.github.com>
2025-05-15 13:55:48 +0900

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -8,7 +8,6 @@ export default defineAppConfig({
   blog: 'https://tou.kr',
   //description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
   description: 'URL 단축 서비스 / 분석 /서버리스 / 사용자정의 슬러그 / 인공지능 슬러그 / 링크만료 등',
-  description: '',
   //image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
